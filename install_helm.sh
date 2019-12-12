d9aeb853d6d6af690d66b1ef8876ce576257c1e9
Derek Smith <derek.smith@naveego.com>
2019-12-12 14:29:29 -0500

    Added install_helm.sh

---
+++ install_helm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf linux-amd64
+wget -O helm.tar.gz https://get.helm.sh/helm-v2.16.1-linux-amd64.tar.gz
+tar -zxvf helm.tar.gz
+mv ./linux-amd64/helm /usr/local/bin/helm
+/usr/local/bin/helm repo update
